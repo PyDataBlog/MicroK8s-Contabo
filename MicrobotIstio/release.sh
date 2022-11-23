92143b64174f18176fe8af75606a9d56725f01d5
PyDataBlog <pimpfada@gmail.com>
2022-11-23 21:36:35 +0100

    Updated gateway, certificate and release

--- MicrobotIstio/release.sh
+++ MicrobotIstio/release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+kubectl apply -f bot-namespace.yaml
 kubectl label namespace bot-demo istio-injection=enabled --overwrite
+brew install helm
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set installCRDs=true
