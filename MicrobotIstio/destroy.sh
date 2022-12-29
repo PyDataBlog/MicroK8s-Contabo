27e7b8d0c9fd8c9593bedb0eb8bd2efe1d2cee79
PyDataBlog <pimpfada@gmail.com>
2022-12-30 00:53:38 +0100

    Beta release

--- MicrobotIstio/destroy.sh
+++ MicrobotIstio/destroy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kubectl delete -f certificate.yaml -f bot-gateway.yaml -f bot-virtualservice.yaml -f bot-deployment.yaml
+kubectl delete -f bot-deployment.yaml
 kubectl delete ns bot-demo
--- MicrobotIstio/release.sh
+++ MicrobotIstio/release.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 kubectl create namespace bot-demo
 kubectl label namespace bot-demo istio-injection=enabled --overwrite
-# brew install helm
-# helm repo add jetstack https://charts.jetstack.io
-# helm repo update
-# helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set installCRDs=true
+kubectl apply -f bot-deployment.yaml
